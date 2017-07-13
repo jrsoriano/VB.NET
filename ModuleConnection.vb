3a873ae8bd35bcee87d20f8d08afff00ceb8a19b
Jose R. Soriano <jrsoriano@users.noreply.github.com>
2017-07-13 10:20:56 -0500

    Add files via upload

---
+++ ModuleConnection.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.SqlClient
+
+Module ModuleConnection
+    Public connection As SqlConnection 'connection
+    'functon for connection 
+    Sub OpenConnection()
+        Try
+            connection = New SqlConnection("Data Source=N550JK\SQLEXPRESS01; Initial Catalog=Users; Integrated Security=True;")
+            If connection.State = ConnectionState.Closed Then
+                connection.Open()
+            End If
+        Catch ex As Exception
+            MsgBox("Failed to Connect, Error " & ex.ToString)
+        End Try
+    End Sub
+End Module
